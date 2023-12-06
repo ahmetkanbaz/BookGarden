5b557ae4ed13906e1ca320f9c52e4b4e108fcc69
Ahmet KANBAZ <ahmetkanbaz@gmail.com>
2023-12-06 18:45:00 +0300

    BookGarden-Header-Footer

--- bookgarden/src/App.jsx
+++ bookgarden/src/App.jsx
@@ -1,11 +1,15 @@
-import Router from './routes/Router'
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Router from "./routes/Router";
 
 function App() {
   return (
     <>
+      <Header />
       <Router />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
