9b61691b9f117b04180802f6177a9c5c2c2b4d89
Ahmet KANBAZ <ahmetkanbaz@gmail.com>
2023-12-14 15:26:29 +0300

    BookGarden-Theme-Redux
    Proje içerisinde tema değişiminin sağlanması ve her component içerisinde kullanılabilmesi için bir redux yapısı oluşturuldu.

--- bookgarden/src/App.jsx
+++ bookgarden/src/App.jsx
@@ -2,11 +2,33 @@ import { GlobalStyle } from "./GlobalStyle/GlobalStyle";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Router from "./routes/Router";
+import { useEffect } from "react";
+import {useDispatch, useSelector} from 'react-redux'
+import {setTheme} from './redux/slices/theme/themeSlice'
 
 function App() {
+  const dispatch = useDispatch()
+  const theme = useSelector((state) => state.theme.theme)
+
+  const getTheme = () => {
+    const localTheme = localStorage.getItem('bookGardenTheme')
+    if (localTheme) {
+      dispatch(setTheme(JSON.parse(localTheme)))
+    }
+    else {
+      dispatch(setTheme('light'))
+      localStorage.setItem('bookGardenTheme', JSON.stringify('light'))
+    }
+  }
+
+  useEffect(() => {
+    getTheme()
+  }, [])
+
+
   return (
     <>
-      <GlobalStyle />
+      <GlobalStyle theme={theme}/>
       <Header />
       <Router />
       <Footer />
--- bookgarden/src/main.jsx
+++ bookgarden/src/main.jsx
@@ -4,11 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./redux/store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
