52552ce45d3933ef2ac4632d22e31e00e757795a
Ahmet KANBAZ <ahmetkanbaz@gmail.com>
2023-12-14 15:09:39 +0300

    BookGarden-CustomCard-Created

---
+++ bookgarden/src/common/Card/Card.jsx
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+import { CustomCard } from "./CardStyle";
+import { useNavigate } from "react-router-dom";
+
+const Card = ({ movie }) => {
+  const navigate = useNavigate();
+  const { id, imageSrc, title, summary, date } = movie || {};
+  return (
+    <div className="col-lg-2 col-md-3 col-sm-4 col-6">
+      <CustomCard className="card h-100 rounded-2 position-relative">
+        <div onClick={() => navigate(`allbooks/${id}`)}>
+          <img src={imageSrc} className="card-img-top" alt={title} />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{summary}</p>
+          </div>
+          <div className="card-footer text-body-secondary">{date}</div>
+        </div>
+      </CustomCard>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  movie: PropTypes.object,
+};
+
+export default Card;
